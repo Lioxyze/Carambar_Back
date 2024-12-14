7b4766afd15798f6c443a0cea7b3a4824ada4cdd
Lioxyze <lioxyze38190@gmail.com>
2024-12-14 11:35:23 +0100

    Swaager Update Dto

--- src/blague/dto/create-blague.dto.ts
+++ src/blague/dto/create-blague.dto.ts
@@ -1,9 +1,19 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBlagueDto {
+  @ApiProperty({
+    description: 'Le texte de la blague',
+    example:
+      "Pourquoi les plongeurs plongent-ils en arrière ? Parce qu'ils tombent dans l'eau !",
+  })
   @IsString()
   blagues: string;
 
+  @ApiProperty({
+    description: 'La réponse à la blague',
+    example: 'Parce que sinon ils tombent dans le bateau.',
+  })
   @IsString()
   reponse: string;
 }
