420daca8d86c8d25a10987ace1b0d52373ee52fe
Lioxyze <lioxyze38190@gmail.com>
2024-12-14 11:27:18 +0100

    test swagger

--- src/blague/blague.controller.ts
+++ src/blague/blague.controller.ts
@@ -1,22 +1,47 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { BlagueService } from './blague.service';
 import { CreateBlagueDto } from './dto/create-blague.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
 
+@ApiTags('Blagues')
 @Controller('')
 export class BlagueController {
   constructor(private readonly blagueService: BlagueService) {}
 
   @Get('/blague')
+  @ApiOperation({ summary: 'Récupérer toutes les blagues' })
+  @ApiResponse({
+    status: 200,
+    description: 'Liste des blagues récupérée avec succès.',
+  })
+  @ApiResponse({ status: 404, description: 'Liste des blagues non récupérée.' })
   getAllBlague() {
     return this.blagueService.getAllBlague();
   }
 
   @Post('/create')
+  @ApiOperation({ summary: 'Créer une nouvelle blague' })
+  @ApiBody({
+    type: CreateBlagueDto,
+    description: 'Les données nécessaires pour créer une nouvelle blague',
+  })
+  @ApiResponse({ status: 201, description: 'Blague créée avec succès.' })
+  @ApiResponse({ status: 404, description: 'Blague non créée.' })
   createBlague(@Body() dto: CreateBlagueDto) {
     return this.blagueService.InsertNewBlague(dto);
   }
 
   @Get('/blague/:id')
+  @ApiOperation({ summary: 'Récupérer une blague par ID' })
+  @ApiParam({ name: 'id', description: "L'identifiant unique de la blague" })
+  @ApiResponse({ status: 200, description: 'Blague récupérée avec succès.' })
+  @ApiResponse({ status: 404, description: 'Blague non trouvée.' })
   GetBlagueViaId(@Param('id') id: string) {
     return this.blagueService.GetBlagueViaId(Number(id));
   }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activer CORS
   app.enableCors();
+
+  // Validation globale
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
 
+  // Configuration de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('CaramBar Fou API')
+    .setDescription(
+      'Une API rapide pour consulter et créer des blagues Carambar.',
+    )
+    .setVersion('1.0')
+    .addTag('Blagues')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api-docs', app, document);
+
+  // Démarrer l'application
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
